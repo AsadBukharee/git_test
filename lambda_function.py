e9404b1f6aef1e2557566d0b8d7457488c2081c5
Asad <asadbukharee@gmail.com>
2023-07-06 16:35:57 +0500

    going into action

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,16 @@
 import json
 from datetime import datetime
+import requests
+
 
 
 def lambda_handler(event, context):
     current_time = datetime.now()
     formatted_time = current_time.strftime("%H-%M-%S : %d-%m")
+
+    # Print the version of the requests library
+
     return {
         'statusCode': 200,
-        'body': json.dumps(f"{formatted_time}")
+        'body': json.dumps(f"Time is :  {formatted_time} and Requests Version is : {requests.__version__}")
     }
